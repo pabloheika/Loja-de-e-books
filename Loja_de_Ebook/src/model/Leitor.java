06624abfe47e822319ae9a2a97131fafc43cec18
pablo <pablo@Gram-de-Pablo>
2022-03-06 20:50:52 -0300

    Em Leitos add atributo userName e seu set e get

--- Loja_de_Ebook/src/model/Leitor.java
+++ Loja_de_Ebook/src/model/Leitor.java
@@ -4,8 +4,18 @@ public class Leitor extends Pessoa{
 	
 	//atributos privados
 	private String dataNascimento;
+	private String userName;
+
 	
 	//gets e sets:
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
 	public String getDataNascimento() {
 		return dataNascimento;
 	}

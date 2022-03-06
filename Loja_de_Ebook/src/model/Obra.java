47700cbd575be8df56b72ac507928f2ec862160b
João Vítor Motta Souto Maior <Jvsoutomaior@users.noreply.github.com>
2022-03-06 18:54:10 -0300

    Model -> model

---
+++ Loja_de_Ebook/src/model/Obra.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Obra {
+	
+	private int id;
+	private String titulo;
+	private String editora;
+	private float edicao;
+	private String autor;
+	private float valor;
+
+}

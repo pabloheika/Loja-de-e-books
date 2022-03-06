c1efc34887a002edbb8c776eccf393c5ef008ae0
pablo <pablo@Gram-de-Pablo>
2022-03-06 20:49:11 -0300

    Em Funcionario add atributo salario e seus gets e sets

--- Loja_de_Ebook/src/model/Funcionario.java
+++ Loja_de_Ebook/src/model/Funcionario.java
@@ -4,7 +4,9 @@ public class Funcionario extends Pessoa {
 	
 	//atributos
 	private String cargo;
+	private long salario;
 
+	
 	//gets e sets:
 	public String getCargo() {
 		return cargo;
@@ -13,5 +15,14 @@ public class Funcionario extends Pessoa {
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
+	
+	public long getSalario() {
+		return salario;
+	}
+
+	public void setSalario(long salario) {
+		this.salario = salario;
+	}
+
 	
 }

9456b3313f9eb2b0319de17001d29ac335b98444
pablo <pablo@Gram-de-Pablo>
2022-03-06 21:23:40 -0300

    inicia os dados das obras em teste

--- Loja_de_Ebook/src/model/teste.java
+++ Loja_de_Ebook/src/model/teste.java
@@ -1,9 +1,21 @@
 package model;
 
 public class teste {
-
+	//inicia objetos
+	static Obra o1 = new Obra();
+	static Obra o2 = new Obra();
+	static Obra o3 = new Obra();
+	static Obra o4 = new Obra();
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//atribui os dados dos objetos
+		o1.setAutor("");
+		o1.setEdicao(1);
+		o1.setEditora("Rocco");
+		o1.setId(0);
+		o1.setTexto("");
+		o1.setTitulo("");
+		o1.setValor(2.5);
+
 
 	}
 

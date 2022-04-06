43614ca89202c46aa4d87c3fd397565c657aed80
pablo <pablo@Gram-de-Pablo>
2022-04-06 04:04:38 -0300

    dasabilita testView

--- Loja_de_Ebook/src/view/testeView.java
+++ Loja_de_Ebook/src/view/testeView.java
@@ -2,12 +2,12 @@ package view;
 
 public class testeView {
 
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 	//classe pra ver como esta ficando
-	new Home();
-	new Login();
-	
+	//new Home();
+	//new Login();
+	//new TelaCadastraFuncionario();
 
-	}
+	//}
 
 }

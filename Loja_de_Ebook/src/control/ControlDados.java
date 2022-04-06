9c652e34f5c29c1c9bd8a5df7df80ce79bbb7b89
pablo <pablo@Gram-de-Pablo>
2022-04-05 21:46:40 -0300

    adiciona contrutor a classe ControlDados

--- Loja_de_Ebook/src/control/ControlDados.java
+++ Loja_de_Ebook/src/control/ControlDados.java
@@ -4,4 +4,16 @@ import model.*;
 
 public class ControlDados {
 	private Dados d = new Dados();
+
+	public ControlDados() {
+		d.geraDados();
+	}
+
+	public Dados getD() {
+		return d;
+	}
+
+	public void setD(Dados d) {
+		this.d = d;
+	}
 }
